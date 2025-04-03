48faa2f49418ed16fa85af316d1814f0b1ebc41b
joneadoerd <joneadoerd@gmail.com>
2025-04-03 22:32:06 +0300

    fix: fix the vercel and prisma auth

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,5 +1,14 @@
+import { routing } from "@/i18n/routing";
 import { createServerClient } from "@supabase/ssr";
+import { getLocale } from "next-intl/server";
 import { type NextRequest, NextResponse } from "next/server";
+
+export const publicRoutes: string[] = [
+  "/",
+  "/sign-in",
+  "forgot-password",
+  "sign-up",
+];
 
 export const updateSession = async (
   request: NextRequest,
@@ -33,21 +42,31 @@ export const updateSession = async (
     // This will refresh session if expired - required for Server Components
     // https://supabase.com/docs/guides/auth/server-side/nextjs
     const user = await supabase.auth.getUser();
-
-    // protected routes
-    if (!request.nextUrl.pathname.startsWith("/") && user.error) {
-      return NextResponse.redirect(new URL("/sign-in", request.url));
+    const { nextUrl } = request;
+    const { locales } = routing;
+    const publicPathnameRegex = RegExp(
+      `^(/(${locales.join("|")}))?(${publicRoutes
+        .flatMap((p) => (p === "/" ? ["", "/"] : p))
+        .join("|")})/?$`,
+      "i"
+    );
+    const isPublicRoute = publicPathnameRegex.test(nextUrl.pathname);
+    if (!isPublicRoute && user.error) {
+      return NextResponse.redirect(new URL(`/sign-in`, request.url));
     }
 
-    if (request.nextUrl.pathname === "/" && !user.error) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
+    // if (request.nextUrl.pathname === "/" && !user.error) {
+    //   return NextResponse.redirect(
+    //     new URL(`/${locale || routing.defaultLocale}`, request.url)
+    //   );
+    // }
 
     return response;
   } catch (e) {
     // If you are here, a Supabase client could not be created!
     // This is likely because you have not set up environment variables.
     // Check out http://localhost:3000 for Next Steps.
+    console.error("Failed to create Supabase client: ", e);
     return NextResponse.next({
       request: {
         headers: request.headers,
